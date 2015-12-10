02367b88105062099be8bef0886f304f1de80b27
Tomilov Anatoliy <tomilovanatoliy@gmail.com>
2015-12-10 11:18:33 +0500

    #

--- test/eggs_variant.cpp
+++ test/eggs_variant.cpp
@@ -13,11 +13,15 @@ main()
     using ::test::literal_type;
     using ::test::common_type;
     using ::test::check_indexing;
+    using ::test::check_destructible;
     using ::test::perferct_forwarding;
     { // eggs::variant
         using ::test_eggs_variant::eggs_variant_c;
         {
             ASSERT ((check_indexing< identity, eggs_variant_c >::run()));
+        }
+        {
+            //assert ((check_destructible< identity, eggs_variant_c >::run())); // clang-3.8: fatal error: unable to execute command: Segmentation fault (core dumped)
         }
         {
             ASSERT ((perferct_forwarding< literal_type, eggs_variant_c, identity, 2, 2 >::run()));
